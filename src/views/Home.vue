9f0b76ddaec313545e377e49714f2b22a9ba535c
Miro <MiroPhoenix91@gmail.com>
2021-02-25 11:16:13 +0100

    vue router testing

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore minima, officia rerum at nostrum expedita sed qui animi architecto illo recusandae culpa dignissimos. Aspernatur impedit asperiores praesentium dolores aliquid ipsam, possimus atque molestiae nihil magni, laboriosam similique. Consectetur amet nihil distinctio sunt eum sapiente voluptatibus beatae praesentium corrupti, assumenda a eos earum molestiae? Repellat ut recusandae iure quam nostrum eius id qui, consequatur exercitationem delectus quis quas ea magni facilis voluptas at cum quisquam, culpa saepe eaque harum unde quidem? Nihil repudiandae quis blanditiis explicabo distinctio accusantium animi quod ipsa autem fuga! Eveniet voluptate voluptatem placeat modi libero, hic pariatur.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore minima, officia rerum at nostrum expedita sed qui animi architecto illo recusandae culpa dignissimos. Aspernatur impedit asperiores praesentium dolores aliquid ipsam, possimus atque molestiae nihil magni, laboriosam similique. Consectetur amet nihil distinctio sunt eum sapiente voluptatibus beatae praesentium corrupti, assumenda a eos earum molestiae? Repellat ut recusandae iure quam nostrum eius id qui, consequatur exercitationem delectus quis quas ea magni facilis voluptas at cum quisquam, culpa saepe eaque harum unde quidem? Nihil repudiandae quis blanditiis explicabo distinctio accusantium animi quod ipsa autem fuga! Eveniet voluptate voluptatem placeat modi libero, hic pariatur.</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
